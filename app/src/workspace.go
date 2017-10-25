1d787884e0b1377153ed56458afc76da35fd02f7
xach <zironside@hotmail.com>
2017-10-24 22:10:01 -0400

    fix command

--- app/src/workspace.go
+++ app/src/workspace.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 // Workspace as defined
@@ -20,22 +21,35 @@ type Workspace struct {
 // Start starts Workspaces
 func (w *Workspace) Start() {
 	config := GetConfig()
-	var buffer bytes.Buffer
-	buffer.WriteString(fmt.Sprintf("run --name %s_%s ", config.Namespace, w.Name))
+	args := []string{}
+	args = append(args, "run")
+	args = append(args, "--name")
+	args = append(args, fmt.Sprintf("%s_%s", config.Namespace, w.Name))
 
 	for _, value := range w.Ports {
-		buffer.WriteString(fmt.Sprintf("-p 0:%d ", value))
+		args = append(args, "-p")
+		args = append(args, fmt.Sprintf("0:%d", value))
 	}
 
 	for key, value := range w.Environment {
-		buffer.WriteString(fmt.Sprintf("-e %s=%s ", key, value))
+		args = append(args, "-e")
+		args = append(args, fmt.Sprintf("%s=%s", key, value))
 	}
 
-	buffer.WriteString(fmt.Sprintf("-v %s/%s:%s %s", config.GetVolumeDir(), w.Name, w.Volume, w.Image))
+	args = append(args, "-v")
+	args = append(args, fmt.Sprintf("%s/%s:%s", config.GetVolumeDir(), w.Name, w.Volume))
+	args = append(args, fmt.Sprintf("%s", w.Image))
 
-	fmt.Println(buffer.String())
-	if err := exec.Command("cat", "../").Run(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	fmt.Println(strings.Join(args, ", "))
+	cmd := exec.Command("docker", args...)
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
 		os.Exit(1)
 	}
+	fmt.Println(out.String())
 }
