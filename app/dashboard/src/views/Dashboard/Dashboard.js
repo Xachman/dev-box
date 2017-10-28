9350459d4e8fe7cf4fd62bb870a5cfeff14999b1
xach <zironside@hotmail.com>
2017-10-28 07:32:06 -0400

    add dashboard

---
+++ app/dashboard/src/views/Dashboard/Dashboard.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Workspace from './Workspace';
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {workspaces:[]}
+  }
+  componentDidMount() {
+    this.getWorkspaces();
+  }
+
+  getWorkspaces() {
+    fetch("http://192.168.1.150:9080/workspaces").then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({ workspaces: responseJson });
+    })
+  }
+  render() {
+    return (
+      <div className="animated fadeIn">
+        {this.state.workspaces.map(function(item, index) {
+          console.log("item", item);
+          return <Workspace key={index} name={item.Name} />
+        })}
+      </div>
+    )
+  }
+}
+
+export default Dashboard;
---
+++ app/dashboard/src/views/Dashboard/Workspace.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Workspace extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: props.name,
+            status: ""
+        }
+        this.getStatus(this.state.name)
+    }
+    getStatus(id) {
+        fetch("http://192.168.1.150:9080/workspaces/status/"+id).then((response) => response.json())
+        .then((responseJson) => {
+        this.setState({ status: responseJson.Status });
+        })
+
+    }
+    startContainer() {
+        fetch("http://192.168.1.150:9080/workspaces/start/"+this.state.name,{method: "POST"}).then(() => {
+            this.getStatus(this.state.name)
+        })
+    }
+    stopContainer() {
+        fetch("http://192.168.1.150:9080/workspaces/stop/"+this.state.name, {method:"POST"}).then(() => this.getStatus(this.state.name))
+    }
+    render() {
+        console.log("state", this.state)
+        return (
+        <div className="workspace">
+            <span className="Name">{this.state.name}</span> &nbsp;
+            <span className="status">{this.state.status}</span> &nbsp;
+            <button className="start btn btn-primary" onClick={this.startContainer.bind(this)}>Start</button> &nbsp;
+            <button className="stop btn btn-primary" onClick={this.stopContainer.bind(this)}>Stop</button> &nbsp;
+        </div>
+        )
+    }
+    }
+Workspace.propTypes = {
+    name: PropTypes.string.isRequired
+}
+
+export default Workspace;
