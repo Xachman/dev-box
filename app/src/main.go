9d8d3b345fe4ff51fee51996da277601b39682d1
xach <zironside@hotmail.com>
2017-10-28 14:09:14 -0400

    add console api

--- app/src/main.go
+++ app/src/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"strings"
+
+	"golang.org/x/net/websocket"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -20,13 +22,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func main() {
-	http.HandleFunc("/", handler)
 	workspaceController := WorkspaceController{
 		DataDir: "../data/workspaces",
 	}
+	consoleController := ConsoleController{}
+
+	http.Handle("/workspaces/exec/", websocket.Handler(consoleController.ExecContainer))
 	http.HandleFunc("/workspaces/start/", workspaceController.StartContainer)
 	http.HandleFunc("/workspaces/stop/", workspaceController.StopContainer)
 	http.HandleFunc("/workspaces/status/", workspaceController.ContainerStatus)
 	http.HandleFunc("/workspaces", workspaceController.Index)
+	http.Handle("/", http.FileServer(http.Dir("./public")))
 	http.ListenAndServe(":9080", nil)
 }
