98258a0fcbec09f3ab46f6223432a9682d36506b
Xach <zironside@hotmail.com>
2017-12-02 13:25:33 -0500

    update config to relative when building

--- app/dashboard/src/Config.js
+++ app/dashboard/src/Config.js
@@ -1,9 +1,13 @@
 export default class Config {
+static get relative() { return true }
     static get devBoxHost(){  return "localhost"}
     static get devBoxPort(){ return 9080 }
     static get protocol() { return "http" }
 
     static dockerHostUrl() {
+        if(this.relative) {
+            return "/";
+        }
         return this.protocol+"://"+this.devBoxHost+":"+this.devBoxPort+"/";
     }
     static host() {
