96f7d3abdf649ecd17ad6e7abbc820e055d9ddd4
xachman <zironside@hotmail.com>
2018-01-16 19:51:12 +0000

    update workspaces layout

--- app/dashboard/src/views/Dashboard/Dashboard.js
+++ app/dashboard/src/views/Dashboard/Dashboard.js
@@ -23,11 +23,14 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="animated fadeIn">
-        <div id="workspaces">
-        {this.state.workspaces.map(function(item, index) {
-          console.log("item", item);
-          return <Workspace key={index} name={item.Name} />
-        })}
+        <div class="col-md-6">
+          <h2>Workspaces</h2>
+          <div id="workspaces">
+          {this.state.workspaces.map(function(item, index) {
+            console.log("item", item);
+            return <Workspace key={index} name={item.Name} />
+          })}
+          </div>
         </div>
       </div>
     )
